3e7a9ffbb02aa7061b364f3751f25437a387bc05
Leeseein <huangha607@naver.com>
2022-07-22 17:45:02 +0900

    plz

--- CalBranch.py
+++ CalBranch.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def mul(a,b):  #곱셈 승렬
     return a*b
 
@@ -9,12 +9,10 @@ def div(a,b): # 나눗셈 원근
 
 
 
-=======
 def add(a,b): # 덧셈 시인
     return a+b
 def sub(a,b): # 뺄셈 시인
     return a-b
->>>>>>> seein
 
 
 def run():  
